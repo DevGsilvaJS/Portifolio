a9fcab992f5c9c53a6a1dae4a7edd989041c89e6
Gabriel Silva <gabriels19961@gmail.com>
2023-06-20 22:27:28 -0300

    v20.06.2023_2

--- WEBApp/Scripts/jsPaginas/jsTipoVenda.js
+++ WEBApp/Scripts/jsPaginas/jsTipoVenda.js
@@ -1,1 +1,68 @@
-﻿
+﻿$(document).ready(function () {
+    jQueryInit();
+});
+
+function jQueryInit() {
+    fnCriaTela();
+}
+
+function fnCriaTela() {
+
+    oTabTipoVenda = $("#tbTipoVenda").DataTable({
+        "oLanguage": {
+            "sProcessing": "Aguarde enquanto os dados são carregados ...",
+            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+            "sPaginationType": "bootstrap",
+            "sZeroRecords": "Nenhum registro correspondente ao critério encontrado",
+            "sInfoEmpty": "Exibindo 0 a 0 de 0 registros",
+            "sInfo": "Exibindo de _START_ a _END_ de _TOTAL_ registros",
+            "sInfoFiltered": "",
+            "sSearch": "Procurar",
+            "oPaginate": {
+                "sFirst": "Primeiro",
+                "sPrevious": "Anterior",
+                "sNext": "Próximo",
+                "sLast": "Último"
+            }
+        },
+
+        "iDisplayLength": 50,
+        "aLengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],
+        "bRetrieve": false,
+        "bFilter": true,
+        "bSortClasses": true,
+        "bLengthChange": false,
+        "bPaginate": true,
+        "bInfo": true,
+        "bJQueryUI": false,
+        "bAutoWidth": false,
+        "aaSorting": [[1, "asc"]],
+        "aoColumns": [
+            { sWidth: '14%', "bSortable": false },
+            { sWidth: '20%' },//Numero          
+            { sWidth: '20%' },//Titulo
+            { sWidth: '20%' },//Titulo   
+            { sWidth: '20%' },//Titulo   
+
+        ]
+    });
+
+
+
+
+    //fnRetornaObjInclusao();
+    //fnListaDados();
+
+    //$(document).ready(function () {
+    //    $("[data-inputmask]").inputmask();
+    //});
+
+
+}
+
+$(document).ready(function () {
+    $('.nav-tabs a').on('click', function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+});
